c10caf8574796e11116871aca19167f6debe5d5f
stein212 <qscaxz0@gmail.com>
2026-04-28 06:37:07 +0000

    Adjust left padding to be none

--- packages/coding-agent/src/modes/interactive/components/branch-summary-message.ts
+++ packages/coding-agent/src/modes/interactive/components/branch-summary-message.ts
@@ -13,7 +13,7 @@ export class BranchSummaryMessageComponent extends Box {
 	private markdownTheme: MarkdownTheme;
 
 	constructor(message: BranchSummaryMessage, markdownTheme: MarkdownTheme = getMarkdownTheme()) {
-		super(1, 1, (t) => theme.bg("customMessageBg", t));
+		super(0, 1, (t) => theme.bg("customMessageBg", t));
 		this.message = message;
 		this.markdownTheme = markdownTheme;
 		this.updateDisplay();
